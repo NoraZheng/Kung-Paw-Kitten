d90e388492b865ed165e1e6a1c896700e382e75a
Nora Zheng <zhengnuo@gmail.com>
2019-08-24 15:00:12 -0400

    save work

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
+//create game object
+const game = {};
+//create a counter variable to track successful hit
+let counter = 0;
+
+//show a random number of fingers each time (1 - 3)
+let fingerNum = Math.ceil(Math.random() * 3);
+
+//create random number between 0 - 8
+const randomIndex = Math.floor(Math.random() * 9);
+
+//method to show finger
+game.showFinger = () => {
+	$(`.finger${randomIndex}`).addClass('showFinger');
+	console.log('a finger shows');
+};
+
+//event listener for fingers on click
+$('.hole').on('click', '.finger', function() {
+	$(this).removeClass('showFinger');
+	counter++;
+	$('.counter').html(counter);
+});
+
 $(document).ready(function() {
 	//document ready
 	console.log('doc ready');
-}); // end of document ready
+	game.showFinger();
+
+	console.log('spawned');
+	setTimeout(escape(), 5000);
+	console.log('cleaned');
+});
